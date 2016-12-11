d1ceb83c2e454b7274047758ddce9662e2f07cf1
Daniel <d_danigarcia@hotmail.es>
2016-12-11 13:55:03 +0100

    Commit inicial

---
+++ README.md
@@ -0,0 +1,89 @@
+# Test SensorTag CC2650
+Test Data Sensor SimpleLink CC2650 SensorTag
+
+### Requerimientos
+##### bluepy
+Interface en Python para dispositivos BLE (Bluetooth Low Energy) sobre Linux.
+[Ver información de la librería](http://ianharvey.github.io/bluepy-doc/)
+[Proyecto Ian Harvey](https://github.com/IanHarvey/bluepy.git)
+[Más documentación](http://www.elinux.org/RPi_Bluetooth_LE)
+
+##### Python 2.7 (Recomendada), 3.4 
+
+##### Sensor SimpleLink SensorTag CC2650
+[Información del Sensor TI](http://www.ti.com/sensortag).
+
+Probad el Tag
+-------------
+Desde vuestro móvil o tablet bajaros la app:
+https://play.google.com/store/apps/details?id=com.ti.ble.sensortag&hl=es
+
+<img src="https://github.com/mfcardenas/test-sl-stagcc2650/blob/master/img/img-slst.png" />
+<img src="https://github.com/mfcardenas/test-sl-stagcc2650/blob/master/img/img-connect.png" style="max-width: 22%;"/>
+<img src="https://github.com/mfcardenas/test-sl-stagcc2650/blob/master/img/img-data-s.png" style="max-width: 22%;"/>
+
+
+Quedaros con la MAC del sensor, cuando os conectéis por Bluetooth la podreís ver (Tenerla siempre a mano).
+
+Lectura del sensor
+------------------
+Haced lo siguiente:
+
+    $ git clone https://github.com/mfcardenas/test-sl-stagcc2650.git
+    $ cd  test-sl-stagcc2650/py
+    $ python get_acceleration.py 00:00:00:00:00
+
+Esto en principio os debería recuperar datos de vuestro sensor.
+ 
+En los directorios /py y /sh tenéis los script que debéis ejecutar para recuperar datos del tag, solo necesitáis indicar la MAC del mismo. 
+Estos script requieren de ciertas "librerías" que se deberían generar en vuestro ordenador pero tal y como está este repositorio no debería ser necesario hacer esa parte porque ya los he generado y les subido aquí.
+
+Si tuvieráis que generar las librerías, os recomiento seguir los pasos que se indican en el apartado siguiente "Generación de librerías".
+
+#### Generación de librerías
+-----------------------
+
+Para generar dichas librerías seguid estos pasos:
+
+##### Instalación bluepy
+------------------
+Se utiliza la librería bluepy para "compilar" o "construir" los script o librerías necesarias que permitirán comunicar nuestro código python con el dispositivo BLE, en este caso el SensorTag.
+
+Bien se puede usar la librería bluepy por defecto, la forma de obtenerla es con "pip".
+- Instale "pip" si no lo tiene en su ordenador y la librería de bluepy. 
+
+    $ sudo apt-get install python-pip libglib2.0-dev
+    $ sudo pip install bluepy
+
+O utilizar la instalación del propio proyecto del autor:
+- instalando localmente los fuentes necesarios para dicha librería.
+
+Si no teneis git:  
+    $ sudo apt-get install git
+
+Si no teneis estas dependencias (suelen ser necesarias):
+    $ sudo apt-get install build-essential libglib2.0-dev
+
+Clonad el proyecto:
+    $ git clone https://github.com/IanHarvey/bluepy.git
+    $ cd bluepy
+    $ python setup.py build
+    $ python setup.py install
+
+
+##### Creación de script para conectar
+--------------------------------------------------
+Cuando instales bluepy bien con pip o bien con la misma librerría del autor, debes construir el proyecto con los fuentes para conectar con el sensor (ver la imagen siguiente)
+
+    $ git clone git clone https://github.com/IanHarvey/bluepy.git
+    $ cd bluepy/bluepy
+    $ make
+
+![Make Project](https://github.com/mfcardenas/test-sl-stagcc2650/blob/master/img/make-install.bmp)
+ 
+Una vez creado el proyecto con make, ejecute el script "btle.py" que se ha generado pasando como argumento la MAC del sensor.
+
+![Execute btle.py](https://github.com/mfcardenas/test-sl-stagcc2650/blob/master/img/get-ingo-sensortag.bmp)
+
+Hecho esto volved al apartado "Lectura del sensor".
+
---
+++ py/recogeDatosSensor.py
@@ -0,0 +1,154 @@
+#
+# TI SimpleLink SensorTag 2016
+# Date: 2016 10 17
+#
+# Sensor: Gyro, Accelerometer and Magnetometer
+# Values: 9 bytes x, y, z for each sensor
+# Note: Sensor values have not been validated
+#
+import struct
+import sys
+import traceback
+import logging
+import time
+import datetime
+import pandas as pd
+import argparse
+
+
+from bluepy.btle import UUID, Peripheral, BTLEException
+
+def TI_UUID(val):
+    return UUID("%08X-0451-4000-b000-000000000000" % (0xF0000000 + val))
+
+
+def obtener_datos_sensor(nombre, tiempo, mac):
+    logging.basicConfig(level=logging.INFO, format='%(levelname)-6s %(asctime)-18s %(message)s', datefmt='%d-%m-%Y %H:%M:%S')
+
+    config_uuid = TI_UUID(0xAA82)
+    data_uuid = TI_UUID(0xAA81)
+
+    # Bit settings to turn on individual movement sensors
+    # bits 0 - 2: Gyro x, y z
+    # bits 3 - 5: Accelerometer x, y, z
+    # bit: 6: Magnetometer turns on X, y , z with one bit
+
+    # gyroOn = 0x0700
+    # accOn = 0x3800
+    # magOn = 0xC001
+
+    # sensorOnVal = gyroOn | magOn | accOn
+    sensorOnVal = 0x7F02
+
+    sensorOn = struct.pack("BB", (sensorOnVal >> 8) & 0xFF, sensorOnVal & 0xFF) #BB dos integers
+    sensorOff = struct.pack("BB", 0x00, 0x00)
+
+    try:
+        logging.info("Trying to connect to: %s", mac)
+        # print "Info, trying to connect to:", sys.argv[1]
+        p = Peripheral(mac)
+
+    except BTLEException:
+        logging.error("Unable to connect!")
+        # print "Fatal, unable to connect!"
+
+    except:
+        logging.error("Unexpected error!")
+        # print "Fatal, unexpected error!"
+        traceback.print_exc()
+        raise
+
+    else:
+
+        try:
+            # print "Info, connected and turning sensor on!"
+            logging.info("Connected and turning sensor ON!")
+            ch = p.getCharacteristics(uuid=config_uuid)[0]
+            ch.write(sensorOn, withResponse=True)
+
+            # print "Info, reading values!"
+            logging.info("Reading values")
+            ch = p.getCharacteristics(uuid=data_uuid)[0]
+
+
+            #GESTION DE DATOS CON PANDAS##########################################################################################
+
+            datos = pd.DataFrame(columns=['timestamp', 'gyro-alpha', 'gyro-beta', 'gyro-gamma', 'accel-x', 'accel-y', 'accel-z'])
+
+            ######################################################################################################################
+
+            print("timestamp;gyro-alpha;gyro-beta;gyro-gamma;accel-x;accel-y;accel-z")
+            tiempo_inicio = round((datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds()* 1000)
+            num_archivo = 0
+            for i in range(0, 50000):
+                rawVals = ch.read()
+                #print "Raw:",
+                for rawVal in rawVals:
+                    temp = ord(rawVal)
+                    #print "%2.2x" % temp,
+                #print
+
+                # Movement data: 9 bytes made up of x, y and z for Gyro, Accelerometer,
+                # and Magnetometer.  Raw values must be divided by scale
+                (gyroX, gyroY, gyroZ, accX, accY, accZ, magX, magY, magZ) = struct.unpack('<hhhhhhhhh', rawVals)
+
+                #scale = 128.0
+                #print "Gyro - x: %2.6f, y: %2.6f, z: %2.6f" % (gyroX / scale, gyroY / scale, gyroZ / scale)
+
+                #scale = 4096.0
+                #print "Acc - x: %2.6f, y: %2.6f, z: %2.6f" % (accX / scale, accY / scale, accZ / scale)
+
+                #scale = (32768.0 / 4912.0)
+                #print "Mag - x: %2.6f, y: %2.6f, z: %2.6f" % (magX / scale, magY / scale, magZ / scale)*/
+
+                timestamp = round((datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds()* 1000)
+
+                #print("%f;%f;%f;%f;%f;%f" % (timestamp, gyroX, gyroY, gyroZ, accX, accY, accZ))
+                scale_accel = 4096.0
+                scale_gyro = 128.0
+                print "%13.0f;%.14f;%.14f;%.14f;%.14f;%.14f;%.14f" %(timestamp, gyroX/scale_gyro, gyroY/scale_gyro, gyroZ/scale_gyro, accX/scale_accel, accY/scale_accel, accZ/scale_accel)
+                #datos.loc[i] = ["%13.0f;%.14f;%.14f;%.14f;%.14f;%.14f;%.14f" %(timestamp, gyroX/scale_gyro, gyroY/scale_gyro, gyroZ/scale_gyro, accX/scale_accel, accY/scale_accel, accZ/scale_accel)]
+                datos.loc[i] = ['{0:13.0f}'.format(timestamp), gyroX/scale_gyro, gyroY/scale_gyro, gyroZ/scale_gyro, accX/scale_accel, accY/scale_accel, accZ/scale_accel]
+                print (timestamp - tiempo_inicio)
+                print ("Vuelta: ", i)
+                if((timestamp - tiempo_inicio ) > tiempo * 1000): #Pasamos tiempo a ms
+                    num_archivo = num_archivo + 1
+                    datos.to_csv("prueba%d.csv" %num_archivo, ';')
+                    tiempo_inicio = round((datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds()* 1000)
+                    datos = pd.DataFrame(columns=['timestamp', 'gyro-alpha', 'gyro-beta', 'gyro-gamma', 'accel-x', 'accel-y', 'accel-z'])
+                    if(num_archivo == 4): #De momento si hemos llegado a completar cuatro archivos, paramos -> se cambiará en el futuro
+                        break;  
+
+            # print "Info, turning sensor off!"
+            datos.to_csv("prueba.csv", ';', index=True)
+            logging.info("Turning sensor OFF")
+            ch = p.getCharacteristics(uuid=config_uuid)[0]
+            ch.write(sensorOff, withResponse=True)
+
+        except:
+            # print "Fatal, unexpected error!"
+            logging.error("Unexpected error!")
+            traceback.print_exc()
+            raise
+
+        finally:
+            # print "Info, disconnecting!"
+            logging.info("Disconnecting...")
+            p.disconnect()
+
+    finally:
+        quit()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Extract features from inputFile and save them in outputFile')
+
+    parser.add_argument("m",
+                        help="MAC del dispositivo")
+    parser.add_argument("n",
+                        help="Nombre del usuario")
+    parser.add_argument("-t", "--t", help="Tiempo en cada uno de los ficheros. Por defecto, 30s",
+                    default=30)
+    args = parser.parse_args()
+
+    obtener_datos_sensor(args.n, args.t, args.m)
