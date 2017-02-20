5b0f6bd1cba2d9834afae759847d0f70b0894c46
vmorante <veronica.morante.pindado@gmail.com>
2017-02-20 23:17:24 +0100

    Version inicial de crear X_train,y_train

---
+++ py/final.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 20 18:56:06 2017
+
+
+"""
+import pandas as pd
+import os.path 
+from os import walk
+def numeroActividad(actividad):
+    if(actividad=="ANDAR"):
+        numero=1
+    elif(actividad=="TROTAR"):
+        numero=2
+    elif(actividad=="BARRER"):
+        numero=3
+    return numero
+                
+    
+    
+
+def juntar():
+     dfOutX = pd.DataFrame()
+     dfOutY = pd.DataFrame()
+    
+    
+     directorioActual=os.getcwd()
+     
+     for (path, ficheros, archivos) in walk("./DatosProcesados"):
+        for i in  archivos:
+            actividad=i.split('-')
+            actividad=actividad[1]
+            os.chdir(directorioActual+"\DatosProcesados")
+            dfX=pd.DataFrame()
+            dfX = pd.read_csv(i, sep=';', index_col=0, error_bad_lines=False)
+            numero=numeroActividad(actividad)
+            numeroFilas=len(dfX)
+            dfY=pd.DataFrame()
+            for i in range(numeroFilas):
+                dfY[i]=numero
+        dfOutX = pd.concat([dfOutX, dfX])
+        dfOutY = pd.concat([dfOutY, dfY])
+     os.chdir(directorioActual)
+     dfOutX.to_csv("X_train.csv")
+     dfOutY.to_csv("y_train.csv")
+            
+if __name__ == "__main__":
+    juntar()
+            
+        
+    
