6d85d08b0b8b1aa7f29a68f98cc02f4916fd296a
Daniel <d_danigarcia@hotmail.es>
2017-02-20 17:54:48 +0100

    Modificado cuerpo del bucle al segmentar datos. Ahora, no se hace if dentro del bucle

--- py/ProcesarDatos.py
+++ py/ProcesarDatos.py
@@ -4,7 +4,16 @@ import argparse
 def getStatisticsValues(nombre, numeroFicheros,time=1, overlap=500):
     dfOut = pd.DataFrame()
     
-    for i in range(0, int(numeroFicheros)):
+    dfRollingMean = pd.DataFrame(columns=['avg_gyro-alpha', 'avg_gyro-beta', 'avg_gyro-gamma', 'avg_ax', 'avg_ay', 'avg_az']);
+
+    dfRollingMin = pd.DataFrame(columns=['min_gyro-alpha', 'min_gyro-beta', 'min_gyro-gamma', 'min_ax', 'min_ay', 'min_az']);
+        
+    dfRollingMax = pd.DataFrame(columns=['max_gyro-alpha', 'max_gyro-beta', 'max_gyro-gamma', 'max_ax', 'max_ay', 'max_az']);
+    
+    dfOut = dfRollingMean.T.append(dfRollingMin.T).append(dfRollingMax.T)
+    dfOut = dfOut.T    
+    
+    for i in range(0, numeroFicheros):
         df = pd.read_csv("%s-%d.csv" %(nombre, i+1), sep=';', index_col=0, error_bad_lines=False)
         dfConjunta = pd.DataFrame();
         df.index = pd.to_datetime(df.index.values, unit='ms')
@@ -21,13 +30,9 @@ def getStatisticsValues(nombre, numeroFicheros,time=1, overlap=500):
         dfRollingMax = dfResampleMax.rolling('%ds' %(time)).max()
         dfRollingMax.columns = ['max_gyro-alpha', 'max_gyro-beta', 'max_gyro-gamma', 'max_ax', 'max_ay', 'max_az']
 
-        if(i > 0):
-            dfConjunta = dfRollingMean.T.append(dfRollingMin.T).append(dfRollingMax.T)
-            dfConjunta = dfConjunta.T
-            dfOut = pd.concat([dfOut, dfConjunta])
-        else:
-            dfOut = dfRollingMean.T.append(dfRollingMin.T).append(dfRollingMax.T)
-            dfOut = dfOut.T
+        dfConjunta = dfRollingMean.T.append(dfRollingMin.T).append(dfRollingMax.T)
+        dfConjunta = dfConjunta.T
+        dfOut = pd.concat([dfOut, dfConjunta])
         
     
     dfOut.to_csv("%s-procesado.csv" %(nombre), ';')
