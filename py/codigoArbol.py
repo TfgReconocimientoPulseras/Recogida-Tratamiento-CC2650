036b84e351b104ff13a5b234e0aea963f2a9b734
Apidal <alex.pidal@hotmail.com>
2017-04-20 20:25:25 +0200

    El script ya genera el codigo con todas las caracteristicas bien

--- py/codigoArbol.py
+++ py/codigoArbol.py
@@ -13,6 +13,13 @@ y = genfromtxt("y_train.csv", delimiter='')
 clf = DecisionTreeClassifier(criterion='entropy', max_depth=6, random_state=0)
 clf = clf.fit(X, y)
 
+def maximovalor(arr):
+    maximo = 0
+    for i in range(len(arr)):
+        if(arr[i] > arr[maximo]):
+            maximo = i
+    return maximo
+
 def tree_to_code(tree, feature_names):
     tree_ = tree.tree_
     feature_name = [
@@ -31,14 +38,12 @@ def tree_to_code(tree, feature_names):
             print "{}else //if({} > {})".format(indent, name, threshold)
             recurse(tree_.children_right[node], depth + 1)
         else:
-            if tree_.value[node][0][0] > tree_.value[node][0][1]:
-                print "{}return {};".format(indent, int(tree.classes_[0]))
-            else:
-                print "{}return {};".format(indent, int(tree.classes_[1]))
+            a = maximovalor(tree_.value[node][0])
+            print "{}return {};".format(indent, int(a + 1))
         
     recurse(0, 1)
 
 
-feature_names=['avg_gyro-alpha', 'avg_gyro-beta', 'avg_gyro-gamma', 'avg_ax', 'avg_ay', 'avg_az', 'min_gyro-alpha', 'min_gyro-beta', 'min_gyro-gamma', 'min_ax', 'min_ay', 'min_az', 'max_gyro-alpha', 'max_gyro-beta', 'max_gyro-gamma', 'max_ax', 'max_ay', 'max_az']
+feature_names=[ 'gyro_alpha_avg','gyro_beta_avg','gyro_gamma_avg','accel_x_avg','accel_y_avg','accel_z_avg','gyro_alpha_min','gyro_beta_min','gyro_gamma_min','accel_x_min','accel_y_min','accel_z_min','gyro_alpha_max','gyro_beta_max','gyro_gamma_max','accel_x_max','accel_y_max','accel_z_max','gyro_alpha_std','gyro_beta_std','gyro_gamma_std','accel_x_std','accel_y_std','accel_z_std','xy_cor','xz_cor','yz_cor','x_fft','y_fft','z_fft','gyro_alpha_med','gyro_beta_med','gyro_gamma_med','accel_x_med','accel_y_med','accel_z_med']
 
 tree_to_code(clf,feature_names)
